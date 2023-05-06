fbfb048afaba620740c508b4158529f85198369a
2yunseong <dbsdltjd3701@naver.com>
2023-05-06 18:37:18 +0900

    feat: add typescript

--- dist/components/BottomSheet.js
+++ dist/components/BottomSheet.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import './BottomSheet.css';
-import { createPortal } from 'react-dom';
-export const BottomSheet = ({ children, isOpen, onToggleOpen, }) => {
-    return (React.createElement(React.Fragment, null, isOpen &&
-        createPortal(React.createElement("div", { className: 'bottom-sheet' },
-            React.createElement("div", { className: 'back-drop', onClick: onToggleOpen }),
-            React.createElement("div", { className: 'bottom-sheet-body' }, children)), document.body)));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BottomSheet = void 0;
+const react_1 = __importDefault(require("react"));
+require("./BottomSheet.css");
+const react_dom_1 = require("react-dom");
+const BottomSheet = ({ children, isOpen, onToggleOpen, }) => {
+    return (react_1.default.createElement(react_1.default.Fragment, null, isOpen &&
+        (0, react_dom_1.createPortal)(react_1.default.createElement("div", { className: 'bottom-sheet' },
+            react_1.default.createElement("div", { className: 'back-drop', onClick: onToggleOpen }),
+            react_1.default.createElement("div", { className: 'bottom-sheet-body' }, children)), document.body)));
+};
+exports.BottomSheet = BottomSheet;
