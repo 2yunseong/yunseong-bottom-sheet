92857ed677e784d09b3cbb2cbffbf0c32df7b3f4
2yunseong <dbsdltjd3701@naver.com>
2023-05-06 20:45:59 +0900

    change jsx config

--- dist/components/BottomSheet.js
+++ dist/components/BottomSheet.js
@@ -1,16 +1,11 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BottomSheet = void 0;
-const react_1 = __importDefault(require("react"));
+const jsx_runtime_1 = require("react/jsx-runtime");
 require("./BottomSheet.css");
 const react_dom_1 = require("react-dom");
 const BottomSheet = ({ children, isOpen, onToggleOpen, }) => {
-    return (react_1.default.createElement(react_1.default.Fragment, null, isOpen &&
-        (0, react_dom_1.createPortal)(react_1.default.createElement("div", { className: 'bottom-sheet' },
-            react_1.default.createElement("div", { className: 'back-drop', onClick: onToggleOpen }),
-            react_1.default.createElement("div", { className: 'bottom-sheet-body' }, children)), document.body)));
+    return ((0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, { children: isOpen &&
+            (0, react_dom_1.createPortal)((0, jsx_runtime_1.jsxs)("div", Object.assign({ className: 'bottom-sheet' }, { children: [(0, jsx_runtime_1.jsx)("div", { className: 'back-drop', onClick: onToggleOpen }), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: 'bottom-sheet-body' }, { children: children }))] })), document.body) }));
 };
 exports.BottomSheet = BottomSheet;
