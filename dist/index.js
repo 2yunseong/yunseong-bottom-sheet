e8818be10f22dc56171be8c80383f70f234c8b7e
2yunseong <dbsdltjd3701@naver.com>
2023-05-06 18:50:05 +0900

    refactor: change module system

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,5 @@
-export { BottomSheet } from './components/BottomSheet';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BottomSheet = void 0;
+var BottomSheet_1 = require("./components/BottomSheet");
+Object.defineProperty(exports, "BottomSheet", { enumerable: true, get: function () { return BottomSheet_1.BottomSheet; } });
