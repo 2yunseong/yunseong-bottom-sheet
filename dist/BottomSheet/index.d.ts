4a7bc7da9beafc3a6c31e96202c6d5fd7b8ac1b9
2yunseong <dbsdltjd3701@naver.com>
2023-05-07 16:26:04 +0900

    remove deprecate file

--- dist/BottomSheet/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react';
-import './BottomSheet.css';
-type BottomSheetProps = {
-    isOpen: boolean;
-    onToggleOpen: () => void;
-};
-export declare const BottomSheet: ({ children, isOpen, onToggleOpen, }: PropsWithChildren<BottomSheetProps>) => JSX.Element;
-export {};
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,8 @@
-export { BottomSheet } from './BottomSheet';
+import { PropsWithChildren } from 'react';
+import './BottomSheet.css';
+type BottomSheetProps = {
+    isOpen: boolean;
+    onToggleOpen: () => void;
+};
+export declare const BottomSheet: ({ children, isOpen, onToggleOpen, }: PropsWithChildren<BottomSheetProps>) => JSX.Element;
+export {};
